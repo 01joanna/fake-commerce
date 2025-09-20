526ffa5c1501f7584d3ee851157efa1124674a92
01joanna <joa.cuevas9@gmail.com>
2025-09-20 16:26:31 +0200

    fixed errors

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 interface NavBarProps {
-    value: string,
-    onChange: (newValue: string) => void
+    value: string;
+    onChange: (newValue: string) => void;
 }
 
-export default function Navbar({ value, onChange }: NavBarProps) {
+export default function NavBar({ value, onChange }: NavBarProps) {
     return (
         <header className="h-auto w-screen items-center flex flex-col">
             <nav>
@@ -16,13 +16,13 @@ export default function Navbar({ value, onChange }: NavBarProps) {
                 </ul>
             </nav>
 
-            <input 
-            type="text"
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            placeholder='Buscar productos'
-            className='border border-black w-96 px-5 py-2 rounded-lg'
+            <input
+                type="text"
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+                placeholder="Buscar productos"
+                className="border border-black w-96 px-5 py-2 rounded-lg"
             />
         </header>
-    )
+    );
 }
--- src/pages/ProductsList.tsx
+++ src/pages/ProductsList.tsx
@@ -1,7 +1,14 @@
+import { useProducts } from "../hooks/useProducts"
+import Card from "../components/Card/Card"
+
 export default function ProductsList() {
+
+    const { products, error } = useProducts()
+    console.log(products)
+    
     return (
         <div>
-            ProductsList
+
         </div>
     )
 }
